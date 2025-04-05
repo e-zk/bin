e24917db8856f23cea9d006af11da86f2a3868fd
e-zk <e-zk@users.noreply.github.com>
2025-04-05 15:39:25 +1000

    delete: stuff i don't use
    netstop: fix typo

--- img/blur.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# blur image/video
-
-INPUT="$1"
-SIGMA="$2"
-STEPS="$3"
-OUTPUT="$4"
-
-ffmpeg -i "$INPUT" -filter_complex "gblur=sigma=${SIGMA}:steps=${STEPS}" "$OUTPUT"
--- img/invert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# invert an image/video
-
-INPUT="$1"
-OUTPUT="$2"
-
-ffmpeg -i "$INPUT" -vf negate "$OUTPUT"
